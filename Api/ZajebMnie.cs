06dc7c36a50566fa3186d9dcf3f17233bc50faa8
Kynes <kamilpirog98@gmail.com>
2023-07-07 13:20:57 +0200

    zajeb mnie

---
+++ Api/ZajebMnie.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using BlazorApp.Shared;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace ApiIsolated
+{
+    public class ZajebMnie
+    {
+        private readonly ILogger _logger;
+
+        public ZajebMnie(ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger<ZajebMnie>();
+        }
+
+        [Function("Zajebmnie")]
+        public HttpResponseData Run([HttpTrigger(AuthorizationLevel.Anonymous, "get")] HttpRequestData req)
+        {
+            var randomNumber = new Random();
+            var temp = 0;
+
+            var result = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = temp = randomNumber.Next(-20, 55),
+                Summary = GetSummary(temp)
+            }).ToArray();
+
+            var response = req.CreateResponse(HttpStatusCode.OK);
+            response.WriteAsJsonAsync(result);
+
+            return response;
+        }
+
+        private string GetSummary(int temp)
+        {
+            var summary = "Mild";
+
+            if (temp >= 32)
+            {
+                summary = "Hot";
+            }
+            else if (temp <= 16 && temp > 0)
+            {
+                summary = "Cold";
+            }
+            else if (temp <= 0)
+            {
+                summary = "Freezing";
+            }
+
+            return summary;
+        }
+    }
+}
