1a75455fafd86ed97e9260586a83d5630620db4c
Kynes <kamilpirog98@gmail.com>
2023-07-07 12:29:04 +0200

    jebac zycie2

--- Api/WeatherForecastFunction.cs
+++ Api/WeatherForecastFunction.cs
@@ -1,10 +1,16 @@
 using System;
+using System.IO;
 using System.Linq;
 using System.Net;
+using System.Threading.Tasks;
 using BlazorApp.Shared;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 
 namespace ApiIsolated
 {
@@ -34,6 +40,26 @@ namespace ApiIsolated
             response.WriteAsJsonAsync(result);
 
             return response;
+        }
+        
+        [FunctionName("HelloYou")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+            
+            string name = req.Query["name"];
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            name = name ?? data?.name;
+
+            string responseMessage = string.IsNullOrEmpty(name)
+                ? "This SECURED HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
+                : $"Bonjour Hi, {name}. This SECURED HTTP triggered function executed successfully.";
+
+            return new OkObjectResult(responseMessage);
         }
 
         private string GetSummary(int temp)
